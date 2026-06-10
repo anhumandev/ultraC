0ffd837929fbc18ac5ecef7275ff14de3bdaa7c6
AHUMAN <pouya.momhidei@gmail.com>
2026-06-10 14:35:59 +0330

    Create helloworld.c

---
+++ testfiles/helloworld.c
@@ -0,0 +1,11 @@
+//Support UltraC v0.1 style.
+
+int main() {
+  printf("hello world!\n");
+  return 0;
+}
+
+//super importnet! : use _ss to call main.
+//without _ss, program won't run.
+
+int _ss = main();
